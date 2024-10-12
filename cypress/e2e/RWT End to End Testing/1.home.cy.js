47f6ccc42c1282da95bdf093cfcf8cc2d691788f
shubhams2987 <shubhams2987@gmail.com>
2024-10-12 15:40:40 +0530

    hi

--- cypress/e2e/RWT End to End Testing/1.home.cy.js
+++ cypress/e2e/RWT End to End Testing/1.home.cy.js
@@ -45,6 +45,7 @@ describe('home page ', () => {
       // })
 
     })
+    //mo
 
     it('Testing your Testing Foundation',()=>{
       cy.getByData('course-1').find('a').contains('Get started').click()
